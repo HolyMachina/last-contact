3260063e640194e264055433e9dbcdae302dd1f3
Josie Troiani <jtroiani42@gmail.com>
2024-11-12 11:32:44 -0600

    build prmary nav structure

--- pages/_meta.js
+++ pages/_meta.js
@@ -1,3 +1,6 @@
 export default {
   index: {title: "Home", type: "page"},
+  lastcontact: {title: "Last Contact", type: "page"},
+  theblog: {title: "Josie's Ramblings", type: "page"},
+  aboutjosie: {title: "About Josie", type: "page"},
 }
