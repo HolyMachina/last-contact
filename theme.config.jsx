c15ed24d4eb30e016fd07f447ac1de3f8c0d7521
Josie Troiani <jtroiani42@gmail.com>
2024-11-17 16:41:27 -0600

    Condense footer and both both links in it.

--- theme.config.jsx
+++ theme.config.jsx
@@ -46,11 +46,9 @@ export default {
   footer: {
     content: (
       <span>
-        <a href="https://last-contact.net/">Last Contact</a> created by <a href="https://last-contact.net/about/aboutjosie">Josephine Troiani</a> 2013 - {new Date().getFullYear()}.<br/>
-        Content is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/?ref=chooser-v1" target="_blank">
-        CC BY-SA 4.0.</a><br/>
-        Website is licened under MIT ©{' '} by <a href="https://nextra.site" target="_blank">Nextra</a>.<br/>
-        Hit me up on <a rel="me" href="https://cutie.city/@Josie">Mastodon</a>.
+        <a href="https://last-contact.net/">Last Contact</a> &#169; {new Date().getFullYear()} by <a href="https://last-contact.net/about/aboutjosie">Josephine Troiani</a> is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">
+        CC BY-SA 4.0</a>.<br/>
+        Hit me up on <a rel="me" href="https://cutie.city/@Josie"  target="_blank">Mastodon</a> or <a href="https://www.linkedin.com/in/jotroian/"  target="_blank">LinkedIn</a>
       </span>
     )
   }
