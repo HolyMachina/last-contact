3f38eac49345f41dc148659e571e8d072ac327a1
Josie Troiani <jtroiani42@gmail.com>
2024-11-24 14:21:14 -0600

    more metadata

--- theme.config.jsx
+++ theme.config.jsx
@@ -14,15 +14,16 @@ export default {
       <>
         <title>{frontMatter.front_matter_title}</title>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta property="og:url" content={url} />
+        <link rel="canonical" href="https://last-contact.net" />
         <meta name="title" content={frontMatter.front_matter_title} />
-        <meta name="og:title" content={frontMatter.front_matter_title} />
         <meta name="author" content={frontMatter.author} />
+        <meta name="copyright" content="Josephine Troiani" />
         <meta name="description" content={frontMatter.description} />
+        <meta property="og:url" content={url} />
+        <meta name="og:title" content={frontMatter.front_matter_title} />
         <meta name="og:description" content={frontMatter.description} />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/favicon.ico" />
-        <meta name="copyright" content="Josephine Troiani" />
         <meta name="robots" content="follow, index" />
         <meta name="twitter:card" content="summary" />
         <meta name="application-name" content="Nextra"/>
@@ -80,9 +81,9 @@ export default {
   footer: {
     content: (
       <span>
-        Hit me up on <a rel="me" href="https://cutie.city/@Josie"  target="_blank">Mastodon</a> or <a href="https://www.linkedin.com/in/jotroian/"  target="_blank">LinkedIn</a>.<br />
-        <a href="https://last-contact.net/">Last Contact</a> &#169; {new Date().getFullYear()} by <a href="https://last-contact.net/about/aboutjosie">Josephine Troiani</a> is licensed under <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">
-        CC BY-SA 4.0</a><br /><Image width={88} height={31} src='https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-sa.svg' alt="Official CC BY-SA press kit banner." />
+        Hit me up on <a rel="me" href="https://cutie.city/@Josie"  target="_blank">Mastodon</a> or <a rel="nofollow"  href="https://www.linkedin.com/in/jotroian/"  target="_blank">LinkedIn</a>.<br />
+        <a href="https://last-contact.net/">Last Contact</a> &#169; {new Date().getFullYear()} by <a href="https://last-contact.net/about/aboutjosie">Josephine Troiani</a> is licensed under <a rel="nofollow" href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">
+        CC BY-SA 4.0</a><br /><Image width={88} height={31} src='https://mirrors.creativecommons.org/presskit/buttons/88x31/svg/by-sa.svg' alt="Official CC BY-SA press kit banner." rel="nofollow" />
       </span>
     )
   }
