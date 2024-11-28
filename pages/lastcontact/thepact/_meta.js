5d5b0dae1b05cdb2e90da9999f30a039dda113e7
Josie Troiani <jtroiani42@gmail.com>
2024-11-28 15:19:40 -0600

    Add Cassandra to the character compendium draft. Update the pact meta to prepare for Hatred Chapter

--- pages/lastcontact/thepact/_meta.js
+++ pages/lastcontact/thepact/_meta.js
@@ -2,6 +2,7 @@ export default {
     discovery: 'Discovery',
     beginning: 'Beginning',
     memories: 'Memories',
+    //hatred: 'Hatred',
     rescue: 'Rescue',
     adjudication: 'Adjudication',
     detection: 'Detection',
