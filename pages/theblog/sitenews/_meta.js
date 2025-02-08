8072ba62e9f8d5dc1df62a10a66265a96ba06737
Josie Troiani <jtroiani42@gmail.com>
2025-02-08 15:48:32 -0600

    New Blog Post: Writing a Big Story

--- pages/theblog/sitenews/_meta.js
+++ pages/theblog/sitenews/_meta.js
@@ -1,5 +1,6 @@
 export default {
     index: "About Site News",
+    writing: "Writing a Big Story",
     itsback: "It's Back",
     notthisweek: 'Not This Week',
     itshappening: "It's Happening"
