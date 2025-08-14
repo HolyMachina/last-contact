4cb96ed72c4c98cb31d30fb1536b9235a661bd05
Josie Troiani <josie@last-contact.net>
2025-08-14 17:22:56 -0500

    Updated Mastodon & Blue Sky Links.

--- app/layout.jsx
+++ app/layout.jsx
@@ -39,10 +39,10 @@ const navbar = (
       <span><b>Last Contact</b><br />The hopepunk web novel!</span>
     </>
     }
-    projectLink='https://bsky.app/profile/last-contact.net'
+    projectLink='https://lgbtqia.space/@holymachina'
     projectIcon={
       <svg width="24" height="24">
-      <image width="24" height="24" href="/images/bluesky-logo.png" alt="Mastodon Logo" />
+      <image width="24" height="24" href="/images/mastodon-logo.png" alt="Mastodon Logo" />
       </svg>
     }
     chatLink='https://discord.gg/kGT7FA7fWr'
@@ -57,7 +57,7 @@ const navbar = (
 const footer = (
   <Footer>
     <span suppressHydrationWarning>
-        Hit me up on <a rel="me" href="https://cutie.city/@Josie">Mastodon</a>, <a href="https://bsky.app/profile/last-contact.net" rel="nofollow">Bluesky</a>, <a href="https://discord.gg/kGT7FA7fWr" rel="nofollow">Discord</a> or <a href="https://www.linkedin.com/in/jotroian" rel="nofollow">LinkedIn</a>.
+        Hit me up on <a rel="me" href="https://lgbtqia.space/@holymachina">Mastodon</a>, <a href="https://bsky.app/profile/lastcontact.space" rel="nofollow">Bluesky</a>, <a href="https://discord.gg/kGT7FA7fWr" rel="nofollow">Discord</a> or <a href="https://www.linkedin.com/in/jotroian" rel="nofollow">LinkedIn</a>.
       <br />
         <a href="https://last-contact.net/">Last Contact</a> &#169; {new Date().getFullYear()} by <a href="https://last-contact.net/about/aboutjosie">Josephine Troiani</a> is licensed under Creative Commons.
       <br />
