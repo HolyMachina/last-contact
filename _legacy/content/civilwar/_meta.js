da3d952f401e24d1e52c1955eb2d3a800c2741a4
Josie Troiani <holymachina@pm.me>
2025-09-25 07:21:52 -0500

    Remove legacy Docs

--- _legacy/content/civilwar/_meta.js
+++
@@ -1,8 +0,0 @@
-export default {
-    backdoor: 'Backdoor',
-    reignite: 'Reignite',
-    representation: 'Representation',
-    projection: 'Projection',
-    thegenerals: 'The Generals',
-    io: 'Io',
-  }
